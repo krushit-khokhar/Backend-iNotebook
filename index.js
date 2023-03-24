044329bcf85cdbdb6980886076ce72bf06857929
krushit <krushit@peanutsquare.com>
2023-03-24 11:00:10 +0530

    create login and register user api with authorization

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 
-const express = require('express')
 const connectToMongo = require('./db');
+const express = require('express')
+// var cors = require('cors') 
 
-
-const app = express();
-const port = 5000;
 connectToMongo();
+const app = express()
+const port = 5000
 
+// app.use(cors())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
